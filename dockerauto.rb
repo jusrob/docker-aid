83d3549e4271168d33839b7b7606a11f73681ea8
220027460 <justin.roberts2@ge.com>
2015-01-27 22:27:38 -0500

    still playing

--- dockerauto.rb
+++ dockerauto.rb
@@ -1,14 +1,25 @@
 #!/usr/bin/ruby
 
+require 'rubygems'
+require 'json'
+
+# set to hardcoded container ID for now
+id = "e00d28c84924"
+
+containerInfo = inspectContainer(id)
+
 class DockerContainer
   def initialize id
     @id = id
+    @mounts = containerInfo[0]["Volumes"]
   end
   def display
-    puts "My Container ID is #{@id}"
+    puts "My Container ID is #{@id} and my volumes are #{@mounts}"
   end
 end
 
-test = DockerContainer.new(5)
-
-test.display
+def inspectContainer(id)
+  info = %x{ docker inspect #{$id}}
+  infohash = JSON.parse(info)
+  return infohash
+end
