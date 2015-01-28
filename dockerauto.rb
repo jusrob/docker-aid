a40e94746e9015c4fd4d0439fc05fdd7fb70116a
220027460 <justin.roberts2@ge.com>
2015-01-27 21:16:58 -0500

    Defining class

--- dockerauto.rb
+++ dockerauto.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/ruby
 
 class DockerContainer
-  
+  def initialize id
+    @id = id
+  end
+  def display
+    puts "My Container ID is #{@id}"
+  end
 end
+
+test = DockerContainer.new(5)
+
+test.display
