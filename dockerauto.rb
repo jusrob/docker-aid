1aa0b07d0630ab5aebbdc833aa94186cb0d97b59
root <root@Yoshi.fallout48.com>
2015-01-27 22:48:55 -0500

    docker inspect code working

--- dockerauto.rb
+++ dockerauto.rb
@@ -3,23 +3,27 @@
 require 'rubygems'
 require 'json'
 
-# set to hardcoded container ID for now
 id = "e00d28c84924"
 
-containerInfo = inspectContainer(id)
-
 class DockerContainer
-  def initialize id
-    @id = id
-    @mounts = containerInfo[0]["Volumes"]
+  def initialize info
+    @info = info
+    @id = info[0]["Config"]["Hostname"]
+    @mounts = info[0]["HostConfig"]["Binds"]
+    @image = info[0]["Config"]["Image"]
   end
   def display
-    puts "My Container ID is #{@id} and my volumes are #{@mounts}"
+    puts "My Container ID is #{@id} from #{@image} and my volumes are #{@mounts}"
   end
 end
 
 def inspectContainer(id)
-  info = %x{ docker inspect #{$id}}
+  info = %x( docker inspect #{id} )
   infohash = JSON.parse(info)
   return infohash
 end
+
+containerInfo = inspectContainer(id)
+
+nzbget = DockerContainer.new(containerInfo)
+nzbget.display
